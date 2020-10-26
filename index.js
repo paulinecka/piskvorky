a6d691096d4407ed01a278b62f486186d76db42d
Pavlina Mullerova <Pauline@pavlinas-macbook-air.home>
2020-10-27 00:35:23 +0100

    skoro, cast stale nefunguje

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 
 let naTahu = 'circle';
 
-const button1 = document.querySelector('.tlacitka button');
+const button1 = document.querySelectorAll('.tlacitka button');
+const kdoHraje = document.querySelector('.ted-hraje');
 
-const hraj = (event) => {
-  if (kdoHraje === 'circle') {
-    event.target.classList.add('board__field--circle');
+const hraj = (udalost) => {
+  kdoHraje.classList.toggle('board__field--circle');
+  if (kdoHraje.classList.contains('board__field--circle')) {
+    udalost.target.classList.add('board__field--circle');
+  } else {
+    udalost.target.classList.add('board__field--cross');
   }
 };
 
@@ -16,8 +20,41 @@ for (let i = 0; i < button1.length; i++) {
 }
 
 /*
+  if ((naTahu = 'circle')) {
+    buttonElm.addEventListener('click', () => {
+      buttonElm.classList.add('board__field--circle');
+    });
+  } else {
+    buttonElm.addEventListener('click', () => {
+      buttonElm.classList.remove('board__field--circle');
+      buttonElm.classList.add('board__field--cross');
+    });
+  }
+
+
+
+const hraj = (event) => {
+  if (kdoHraje.contains('board__field--circle')) {
+    event.target.classList.add('board__field--circle');
+  }
+}; 
+
+for (let i = 0; i < button1.length; i++) {
+  const buttonElm = button1[i];
+  buttonElm.addEventListener('click', () => {
+    console.log('funguju');
+    button1.classList.add('board__field--circle');
+  });
+} 
+
 button1.addEventListener('click', () => {
   console.log('funguju');
   boardField.classList.add('board__field--circle');
+});
+
+
+const kliknu = button1.addEventListener('click', () => {
+  console.log('funguju');
+  button1.classList.add('board__field--circle');
 });
 */
