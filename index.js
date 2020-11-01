5250ab4f2fb1a85d63142a89cf61ecda367fdec9
Pavlina Mullerova <Pauline@Pavlinas-MacBook-Air.local>
2020-11-01 12:49:27 +0100

    vylepseni toggle a reseni vyhravani

--- index.js
+++ index.js
@@ -1,23 +1,113 @@
 'use-strict';
 
-let naTahu = 'circle';
+//let naTahu = 'circle';
 
-const button1 = document.querySelectorAll('.tlacitka button');
+const buttonAll = document.querySelectorAll('.tlacitka button');
 const kdoHraje = document.querySelector('.ted-hraje');
+let player = 'circle';
 
 const hraj = (udalost) => {
-  kdoHraje.classList.toggle('board__field--circle');
   if (kdoHraje.classList.contains('board__field--circle')) {
     udalost.target.classList.add('board__field--circle');
+    player = 'kolecko';
   } else {
     udalost.target.classList.add('board__field--cross');
+    player = 'krizek';
+  }
+  kdoHraje.classList.toggle('board__field--circle');
+  kdoHraje.classList.toggle('board__field--cross');
+
+  console.log('Get position', getPosition(udalost.target));
+  console.log('Get symbol', getSymbol(udalost.target));
+  console.log('is Winning', isWinningMove(udalost.target));
+};
+
+for (let i = 0; i < buttonAll.length; i++) {
+  const buttonElm = buttonAll[i];
+  buttonElm.addEventListener('click', hraj);
+}
+
+const boardSize = 10; // 10x10
+
+const getPosition = (field) => {
+  let fieldIndex = 0;
+  while (fieldIndex < buttonAll.length) {
+    if (field === buttonAll[fieldIndex]) {
+      break;
+    }
+    fieldIndex++;
+  }
+
+  return {
+    row: Math.floor(fieldIndex / boardSize),
+    column: fieldIndex % boardSize,
+  };
+};
+
+const getField = (row, column) => buttonAll[row * 10 + column];
+
+const getSymbol = (field) => {
+  // Název třídy přizpůsob tvému kódu.
+  if (field.classList.contains('board__field--cross')) {
+    return 'cross';
+  } else if (field.classList.contains('board__field--circle')) {
+    return 'circle';
   }
 };
 
-for (let i = 0; i < button1.length; i++) {
-  const buttonElm = button1[i];
-  buttonElm.addEventListener('click', hraj);
-}
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+
+  let i;
+
+  let inRow = 1; // Jednička pro právě vybrané políčko
+  // Koukni doleva
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
+  }
+
+  // Koukni doprava
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+
+  let inColumn = 1;
+  // Koukni nahoru
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+
+  // Koukni dolu
+  i = origin.row;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+
+  if (inColumn >= symbolsToWin) {
+    alert(`Blahopřejeme, uživatel ${player} vyhrál.`);
+  }
+
+  return false;
+};
 
 /*
   if ((naTahu = 'circle')) {
